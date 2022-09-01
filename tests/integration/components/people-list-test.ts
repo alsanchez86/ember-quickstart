f8467893f3e5ecac0e8503f8722e1f705c9ab3d6
Ángel Luis Sánchez Pizarro <sanchezpizarro86@gmail.com>
2022-09-01 17:15:41 +0200

    use Jumbo component

--- tests/integration/components/people-list-test.ts
+++ tests/integration/components/people-list-test.ts
@@ -7,21 +7,8 @@ module('Integration | Component | people-list', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<PeopleList @title='Title' />`);
 
-    await render(hbs`‹PeopleList @title='Title' /›`);
-
-    // assert.equal(this?.element?.textContent, 'Title');
     assert.dom('h2').containsText('Title');
-
-    // // Template block usage:
-    // await render(hbs`
-    //   {{#people-list}}
-    //     template block text
-    //   {{/people-list}}
-    // `);
-
-    // assert.equal(this?.element?.textContent?.trim(), 'template block text');
   });
 });
