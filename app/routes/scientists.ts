f22bc23444e6c1abf7acdd14850719782da21ef2
Ángel Luis Sánchez Pizarro <sanchezpizarro86@gmail.com>
2022-09-12 13:30:50 +0200

    add scss to project

--- app/routes/scientists.ts
+++ app/routes/scientists.ts
@@ -14,5 +14,7 @@ TODO:
 1. Los datos del modelo vendrán de un JSON de científicos (se hará una petición para obtener el json) -> OK
 2. Cuando se clique en el nombre de un científico, mostrar sus otros datos en un modal (en otro componente diferente)
 3. Cuando se clique en otro botón que haya dentro del modal, se cerrará el modal y se hará una petición a un endpoint externo
+4. Hacer que sea posible mediante un input actualizar los datos de los rentals (sus atributos) y que se queden persistidos de forma que sean visibles desde otras rutas.
+5. Mostrar en la ruta de scientists los datos de los rentals (aunque sea los ids) sin necesidad de hacer una nueva petición
 
 */
